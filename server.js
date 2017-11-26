1815678eb36fda1c1129c9ea0cc8149b34d0054c
andregtz <colpac64@gmail.com>
2017-11-25 18:38:47 -0600

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const server = net.createServer(function(socket) {
+  socket.write('Echo server\r\n');
+  socket.pipe(socket);
+
+  socket.on('data', function(data) {
+    var response = data.toString().trim();
+
+    console.log(response);
+  });
+
+  socket.on('end', function() {
+    console.log('client disconnected');
+  });
+});
+
+server.on('error', () => {console.log(error)});
+
+server.listen(1313, '192.168.100.4');
+console.log('Listening at 192.168.100.4:1313');
